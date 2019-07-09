a521027eb4b31762ec588fea3bb31f217b6c65fb
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2019-07-09 20:07:20 -0300

    Add exercise 2A.

---
+++ Topic-3-Javascript/2A.js
@@ -0,0 +1,26 @@
+// ----------------------------- GLOBAL VARIABLES --------------------------- \\
+
+// DOM ELEMENTS
+
+const sayHelloBtn = document.querySelector('button[name="hello"]');
+
+// ------------------------ EVENT HANDLER FUNCTIONS ------------------------- \\
+
+// Executes initialization tasks.
+function initialization(event) {
+  // Shows the hidden section element.
+  const sectionEl = document.querySelector('section');
+  sectionEl.classList.remove('hidden');
+}
+
+function sayHello() {
+  alert('Hello!');
+}
+
+// ------------------------------ EVENT HANDLERS ---------------------------- \\
+
+// initialization() is called when the page finishes loading
+// (both HTML and CSS).
+window.addEventListener('load', initialization);
+
+sayHelloBtn.addEventListener('click', sayHello);
