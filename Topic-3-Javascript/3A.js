cc776e212fdb2aa992eb62edc5d091359c44aa3a
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2019-07-09 20:25:26 -0300

    Update exercise 3A using fetch.

--- Topic-3-Javascript/3A.js
+++ Topic-3-Javascript/3A.js
@@ -6,6 +6,30 @@ const sectionEl = document.querySelector('section');
 const sayHelloBtn = document.querySelector('button[name="hello"]');
 const getChuckJokeBtn = document.querySelector('button[name="joke"]');
 
+// --------------------------------- FUNCTIONS ------------------------------ \\
+
+// Executes a HTTP GET request of JSON data using fetch and async/await.
+// INPUT:
+// <-- url: string ** no type checking is done **
+// The url from which to fetch the JSON data.
+// <-- callback: function ** no type checking is done **
+// The callback function that will receive the response from the HTTP GET
+// request over the said url.
+// OUTPUT:
+// --> void
+// The function worked as expected.
+// --> TypeError
+// NetworkError ocurred when attempting to fetch the resource.
+async function httpGetJSON(url, callback) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    callback(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 // ------------------------ EVENT HANDLER FUNCTIONS ------------------------- \\
 
 // Executes initialization tasks.
@@ -21,13 +45,11 @@ function sayHello() {
 // Writes a random Chuck Norris joke in the h1 element from the section element
 // every time its called.
 function getChuckJoke() {
-  const request = new XMLHttpRequest();
-  request.open('get', 'http://api.icndb.com/jokes/random', true);
-  request.responseType = 'json';
-  request.onload = function() {
-    sectionEl.querySelector('h1').innerHTML = request.response.value.joke;
-  };
-  request.send();
+  httpGetJSON('http://api.icndb.com/jokes/random', data => {
+    if (data.type === 'success')
+      sectionEl.querySelector('h1').innerHTML = data.value.joke;
+    else sectionEl.style.color = 'red';
+  });
 }
 
 // ------------------------------ EVENT HANDLERS ---------------------------- \\
