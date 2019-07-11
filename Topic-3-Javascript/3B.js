3edcb3cc9290d481877e685dd7b3a5180c305678
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2019-07-11 15:07:45 -0300

    Update exercise 3B with debounced search function.

--- Topic-3-Javascript/3B.js
+++ Topic-3-Javascript/3B.js
@@ -1,33 +1,35 @@
-'use strict';
+import debounce from './debounce.js';
 
-// Executes a HTTP GET request of JSON data using fetch and async/await.
+// ----------------------------- GLOBAL VARIABLES --------------------------- \\
+
+// DOM ELEMENTS
+
+const repositoriesEl = document.querySelector('ul');
+const repositorySearchEl = document.querySelector('#search-repository');
+
+// --------------------------------- FUNCTIONS ------------------------------ \\
+
+// Shows a list of repositories matching a given string in a specified list.
 // INPUT:
-// <-- url: string ** no type checking is done **
-// The url from which to fetch the JSON data.
-// <-- callback: function ** no type checking is done **
-// The callback function that will receive the response from the HTTP GET
-// request over the said url.
+// <-- list: object ** no type checking is done **
+// An <ul> (or <ol>) element to put the repositories data as <li>.
+// <-- search: string ** no type checking is done **
+// The search to perform.
 // OUTPUT:
 // --> void
-// The function worked as expected.
-// --> TypeError
-// NetworkError ocurred when attempting to fetch the resource.
-async function httpGetJSON(url, callback) {
+// The repositories matching the "search" string are appended to the specified
+// list.
+async function showRepos(list, search) {
+  const apiErrorEl = document.querySelector('.api-error');
+  apiErrorEl.querySelector('p').innerHTML = '';
   try {
-    const response = await fetch(url);
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${search}`
+    );
     const data = await response.json();
-    callback(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Shows a list of JavaScript repositories on the right side of the page.
-function showJSRepos(search) {
-  const repositories = document.querySelector('ul');
-  httpGetJSON(
-    `https://api.github.com/search/repositories?q=JavaScript`,
-    data => {
+    list.innerHTML = '';
+    if (data.items) {
+      apiErrorEl.style.display = 'none';
       data.items.forEach(repo => {
         // Appends a list of repos as:
         // <li>
@@ -47,10 +49,46 @@ function showJSRepos(search) {
         );
         repository.appendChild(repositoryName);
         repository.appendChild(repositoryDescription);
-        repositories.appendChild(repository);
+        list.appendChild(repository);
       });
+    } else {
+      apiErrorEl.style.display = 'block';
+      const errorMessage = `Sorry, ${
+        data.message && data.message.includes('API rate limit exceeded')
+          ? 'API rate limit exceeded, wait a minute to keep searching.'
+          : 'no repositories could be found!'
+      }.`;
+      apiErrorEl
+        .querySelector('p')
+        .appendChild(document.createTextNode(errorMessage));
     }
-  );
+  } catch (error) {
+    console.error(error);
+    apiErrorEl.style.display = 'block';
+    apiErrorEl
+      .querySelector('p')
+      .appendChild(
+        'Sorry, a network error occurred. Check your internet connection and try again.'
+      );
+  }
 }
 
-showJSRepos('');
+// ------------------------ EVENT HANDLER FUNCTIONS ------------------------- \\
+
+// Executes initialization tasks.
+function initialization() {
+  repositorySearchEl.value = '';
+}
+
+// Debounces showRepos().
+const showReposDebounced = debounce(function(event) {
+  showRepos(repositoriesEl, event.target.value);
+}, 250);
+
+// ------------------------------ EVENT HANDLERS ---------------------------- \\
+
+// initialization() is called when the page finishes loading
+// (both HTML and CSS).
+window.onload = initialization;
+
+repositorySearchEl.addEventListener('input', showReposDebounced);
