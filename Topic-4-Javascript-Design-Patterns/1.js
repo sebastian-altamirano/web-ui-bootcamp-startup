f7ad40c277a8432b6889c4338514f89d7be90639
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2019-07-15 17:12:27 -0300

    Add exercise 1 up to point B (Movie class and instances of it).

---
+++ Topic-4-Javascript-Design-Patterns/1.js
@@ -0,0 +1,14 @@
+class Movie {
+  constructor(name, year, duration) {
+    this.title = name;
+    this.year = year;
+    this.duration = duration;
+  }
+  play() {}
+  pause() {}
+  resume() {}
+}
+
+const antMan = new Movie('Ant-Man', 2015, 118);
+const civilWar = new Movie('Captain America: Civil War', 2016, 148);
+const infinityWar = new Movie('Avengers: Infinity War', 2018, 160);
